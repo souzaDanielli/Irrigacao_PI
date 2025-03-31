c1693f3710af660c84a55e811194c15d3d3e2205
Danielli Silva <164676910+danielli-ssilva@users.noreply.github.com>
2025-03-31 04:52:03 +0000

    server.js: rota post

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ app.get("/api/dados", async (req, res) => {
     }
 });
 
+//rota post
+app.post("/api/dados", async (req, res) => {
+    const { umidadeSolo1, umidadeSolo2, umidadeSolo3, umidadeSolo4, irrigador1, irrigador2, irrigador3, irrigador4 } = req.body;
+    try {
+        const novaLeitura = await prisma.sensor.create({
+            data: { umidadeSolo1, umidadeSolo2, umidadeSolo3, umidadeSolo4, irrigador1, irrigador2, irrigador3, irrigador4 },
+        });
+        res.json(novaLeitura);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao inserir dados" });
+    }
+});
+
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server na porta ${port}`);
 });
